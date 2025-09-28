f41946fc910e9a0dc0c7d45c2b2fcae5ab9a4561
yjot <2010guabo@gmail.com>
2025-09-28 14:22:49 -0500

    Update Responsive 2.1

--- logic.js
+++ logic.js
@@ -1,3 +1,27 @@
+let prevScrollPos = window.pageYOffset;
+
+function handleScroll() {
+  const navbar = document.querySelector(".topbar");
+  let currentScrollPos = window.pageYOffset;
+
+  if (window.innerWidth <= 590) {
+    if (prevScrollPos > currentScrollPos && window.pageYOffset === 0) {
+      // Inicio → mostrar topbar
+      navbar.style.top = "0";
+    } else {
+      // Subiendo o bajando → ocultar topbar
+      navbar.style.top = "-15rem";
+    }
+  } else {
+    // En pantallas grandes, siempre visible
+    navbar.style.top = "0";
+  }
+
+  prevScrollPos = currentScrollPos;
+}
+
+window.addEventListener("scroll", handleScroll);
+
 function googleTranslateElementInit() {
     new google.translate.TranslateElement(
     {pageLanguage: 'es', includedLanguages: 'es,en'},
